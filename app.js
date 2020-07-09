cf9cae51e10ab9ecc5f5a8d8e08ba2411253181d
Matthew Beilsmith <matthewbeilsmith@gmail.com>
2020-07-09 03:40:52 -0500

    testing new draw screen

--- app.js
+++ app.js
@@ -370,14 +370,7 @@ const game = (() => {
     const draw = () => {
       const _draw = document.createElement('div');
       const _message = document.createElement('pre');
-      const _fillPre = () => {
-        let xhr = new XMLHttpRequest();
-        xhr.onload = () => {
-          document.getElementById('message').textContent = this.responseText;
-        };
-        xhr.open('GET', './meow.txt');
-        xhr.send();
-      };
+
       _draw.id = 'end';
       _message.id = 'message';
       // _message.innerText = 'a w  ,  D R A W';
@@ -388,7 +381,12 @@ const game = (() => {
                let _message = document.getElementById('message');
                _message.style.color = 'blanchedalmond';
                _message.style.fontSize = '0.5rem';
-               _fillPre();
+               let xhr = new XMLHttpRequest();
+               xhr.onload = () => {
+                 _message.innerText = this.responseText;
+               };
+               xhr.open('GET', './meow.txt');
+               xhr.send();
              }, 250);
 
     };
